8e8d18df66eb523ba28c0a91ade4e96acbbcedcb
bradyz <brady.zhou@utexas.edu>
2020-06-16 09:06:36 -0500

    update docker

--- scripts/make_docker.sh
+++ scripts/make_docker.sh
@@ -3,28 +3,14 @@
 CARLA_ROOT=/home/bradyzhou/software/CARLA_0.9.9
 SCENARIO_RUNNER_ROOT=/home/bradyzhou/code/2020_CARLA_challenge/scenario_runner
 LEADERBOARD_ROOT=/home/bradyzhou/code/2020_CARLA_challenge/leaderboard
-
-if [ -z "$CARLA_ROOT" ]
-then
-    echo "Error $CARLA_ROOT is empty. Set \$CARLA_ROOT as an environment variable first."
-    exit 1
-fi
-
-if [ -z "$SCENARIO_RUNNER_ROOT" ]
-then echo "Error $SCENARIO_RUNNER_ROOT is empty. Set \$SCENARIO_RUNNER_ROOT as an environment variable first."
-    exit 1
-fi
-
-if [ -z "$LEADERBOARD_ROOT" ]
-then echo "Error $LEADERBOARD_ROOT is empty. Set \$LEADERBOARD_ROOT as an environment variable first."
-    exit 1
-fi
+TEAM_CODE_ROOT=/home/bradyzhou/code/2020_CARLA_challenge/leaderboard/team_code
 
 mkdir .tmp
 
 cp -fr ${CARLA_ROOT}/PythonAPI  .tmp
 cp -fr ${SCENARIO_RUNNER_ROOT}/ .tmp
 cp -fr ${LEADERBOARD_ROOT}/ .tmp
+cp -fr ${TEAM_CODE_ROOT}/ .tmp/team_code
 cp -fr /home/bradyzhou/code/2020_CARLA_challenge/carla_project .tmp
 
 # build docker image
